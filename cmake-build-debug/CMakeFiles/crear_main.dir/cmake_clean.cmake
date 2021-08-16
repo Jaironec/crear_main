1f51247fcd4c688068d593da6d49e49afb98c524
Jaironec <16cardenas16@gmail.com>
2021-08-15 23:08:24 -0500

    crear main perdon por los 10 minutos de atraso se me fue el internet

---
+++ cmake-build-debug/CMakeFiles/crear_main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crear_main.dir/main.cpp.obj"
+  "crear_main.exe"
+  "crear_main.exe.manifest"
+  "crear_main.pdb"
+  "libcrear_main.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crear_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
